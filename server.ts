38a414553fa15ed0d35a6d966d7e7d84648dbfd5
TheJeterLP <joey.peter1998@gmail.com>
2023-01-17 18:57:49 +0100

    init

---
+++ server.ts
@@ -0,0 +1,58 @@
+/**
+ * Required external modules
+ */
+import express, { Application } from 'express';
+import session from 'express-session';
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan, { StreamOptions } from 'morgan';
+
+
+/**
+ * Required internal modules
+ */
+import { info } from './logmanager';
+
+/**
+ * Required configuration sections
+ */
+import { website_port, session_secret } from './config.json';
+
+/**
+ * App Variables
+ */
+const app: Application = express();
+const oneDay = 1000 * 60 * 60 * 24;
+const ads = [
+    {title: 'Hello, world (again)!'}
+  ];
+
+/**
+ * App Configuration
+ */
+app.use(express.json());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(session({
+    secret: session_secret,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+}));
+
+
+app.get('/', (req, res) => {
+    res.send(ads);
+  });
+
+
+/**
+ * Server Activation
+ */
+app.listen(website_port, () => {
+    info(`Listening to requests at 127.0.0.1:${website_port}`);
+});
