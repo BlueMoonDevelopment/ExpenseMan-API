f0cbef69bd0ced41c01db9ffb58e59b0f15a792e
TigerCipher <matt.rogers8396@gmail.com>
2023-01-24 11:21:44 -0600

    docs: Added documentation for category api
    Note that this also includes changing the status sent when no category type was provided when creating a new category to 401 from 400

--- src/main/typescript/routes/categories.routes.ts
+++ src/main/typescript/routes/categories.routes.ts
@@ -6,6 +6,95 @@ import { authJwt } from '../middlewares/authJwt';
 import { Category } from '../models/categories.model';
 
 
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     tags:
+ *     - "Category API"
+ *     description: Brings up all categories for your user if no category_id is specified, or single category
+ *     summary: Get categories
+ *     operationId: categories__get
+ *     parameters:
+ *       - in: header
+ *         name: x-access-token
+ *         schema:
+ *           type: string
+ *         required: true
+ *     requestBody:
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: "object"
+ *             properties:
+ *               account_id:
+ *                 type: "string"
+ *                 example: "63cdbc09a3adb6d82c13254a"
+ *     responses:
+ *       200:
+ *         description: Successful Response
+ *         content:
+ *           application/json:
+ *               schema:
+ *                 type: "array"
+ *                 items:
+ *                   type: "object"
+ *                   properties:
+ *                     _id:
+ *                       title: "Category ID"
+ *                       type: "string"
+ *                     category_owner_id:
+ *                       title: "Owning user ID"
+ *                       type: "string"
+ *                     category_name:
+ *                       title: "Category name"
+ *                       type: "string"
+ *                     category_type:
+ *                       title: "Category type"
+ *                       type: "string"
+ *                     category_desc:
+ *                       title: "Category description"
+ *                       type: "string"
+ *                     category_color:
+ *                       title: "Cateogory color"
+ *                       type: "string"
+ *                     category_symbol:
+ *                       title: "Category Symbol"
+ *                       type: "string"
+ *                     __v:
+ *                       title: "Category version"
+ *                       type: "integer"
+ *                 example:
+ *                 - _id: "63cd6f99810a1500c067a70a"
+ *                   category_owner_id: "63cd40b83391382af2ae71fb"
+ *                   category_name: "Subscriptions"
+ *                   category_type: "Expenses"
+ *                   category_desc: ""
+ *                   category_color: "red"
+ *                   category_symbol: "MoneyBag"
+ *                   __v: 0
+ *                 - _id: "63cd6fbf810a1500c067a70d"
+ *                   category_owner_id: "63cd40b83391382af2ae71fb"
+ *                   category_name: "Work"
+ *                   category_type: "Income"
+ *                   category_desc: "Hourly income from my job"
+ *                   category_color: "green"
+ *                   category_symbol: "MoneyBag"
+ *                   __v: 0
+ *       404:
+ *         description: "Not found"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Error message"
+ *                   type: "string"
+ *             example:
+ *               message: "Specified category_id not found."
+ */
 function registerGetCategoriesFromUser(app: Application) {
     app.get('/categories', authJwt.verifyToken, async (req, res) => {
         try {
@@ -32,7 +121,86 @@ function registerGetCategoriesFromUser(app: Application) {
     });
 }
 
-
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     tags:
+ *     - "Category API"
+ *     summary: "Create new category"
+ *     description: "Create a new category"
+ *     operationId: "categories__post"
+ *     consumes:
+ *     - "application/json"
+ *     parameters:
+ *       - in: header
+ *         name: x-access-token
+ *         schema:
+ *           type: string
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: "object"
+ *             required:
+ *             - category_name
+ *             - category_type
+ *             properties:
+ *               category_name:
+ *                 type: "string"
+ *                 example: "Subscriptions"
+ *               category_type:
+ *                 type: "string"
+ *                 example: "Expenses"
+ *               category_desc:
+ *                 type: "string"
+ *                 example: "Monthly streaming service charges"
+ *               category_color:
+ *                 type: "string"
+ *                 example: "red"
+ *               category_symbol:
+ *                 type: "string"
+ *                 example: "MoneyBag"
+ *     responses:
+ *       200:
+ *         description: "Successful response"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Confirmation message"
+ *                   type: "string"
+ *             example:
+ *               message: "Account creation was successful"
+ *       400:
+ *         description: "No category name provided"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Error message"
+ *                   type: "string"
+ *             example:
+ *               message: "No category name was provided."
+*       401:
+ *         description: "No category type provided"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Error message"
+ *                   type: "string"
+ *             example:
+ *               message: "No category type was provided."
+ */
 function registerCreateCategory(app: Application) {
     app.post('/categories', authJwt.verifyToken, async (req, res, next) => {
         const user_id = mongoose.Types.ObjectId.createFromHexString(req.body.token_user_id);
@@ -43,7 +211,7 @@ function registerCreateCategory(app: Application) {
         }
 
         if (!req.body.category_type) {
-            res.status(400).send({ message: 'No category type was provided.' });
+            res.status(401).send({ message: 'No category type was provided.' });
             return;
         }
 
@@ -63,6 +231,62 @@ function registerCreateCategory(app: Application) {
     });
 }
 
+
+/**
+ * @swagger
+ * /categories:
+ *   delete:
+ *     tags:
+ *     - "Category API"
+ *     summary: "Delete a category"
+ *     description: "Delete a category"
+ *     operationId: "categories__delete"
+ *     consumes:
+ *     - "application/json"
+ *     parameters:
+ *       - in: header
+ *         name: x-access-token
+ *         schema:
+ *           type: string
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: "object"
+ *             required:
+ *             - category_id
+ *             properties:
+ *               category_id:
+ *                 type: "string"
+ *                 example: "f343dfgj435jkgfn34dfdgdf"
+ *     responses:
+ *       200:
+ *         description: "Successful response"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Confirmation message"
+ *                   type: "string"
+ *             example:
+ *               message: "Category deleted successfully"
+ *       404:
+ *         description: "Not found"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Error message"
+ *                   type: "string"
+ *             example:
+ *               message: "No matching category was found for your user."
+ */
 function registerDeleteCategory(app: Application) {
     app.delete('/categories', authJwt.verifyToken, function (req, res, next) {
         const user_id = mongoose.Types.ObjectId.createFromHexString(req.body.token_user_id);
@@ -83,6 +307,77 @@ function registerDeleteCategory(app: Application) {
     });
 }
 
+
+/**
+ * @swagger
+ * /categories:
+ *   put:
+ *     tags:
+ *     - "Category API"
+ *     summary: "Update a category"
+ *     description: "Update a category for your user, category_id is required, everything else is optional."
+ *     operationId: "categories__put"
+ *     consumes:
+ *     - "application/json"
+ *     parameters:
+ *       - in: header
+ *         name: x-access-token
+ *         schema:
+ *           type: string
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: "object"
+ *             required:
+ *             - category_id
+ *             properties:
+ *               category_id:
+ *                 type: "string"
+ *                 example: "63cdbc09a3adb6d82c13254a"
+ *               category_name:
+ *                 type: "string"
+ *                 example: "Subscriptions"
+ *               category_type:
+ *                 type: "string"
+ *                 example: "Expenses"
+ *               category_desc:
+ *                 type: "string"
+ *                 example: "Yearly license charges"
+ *               category_color:
+ *                 type: "string"
+ *                 example: "blue"
+ *               category_symbol:
+ *                 type: "string"
+ *                 example: "BankSymbol"
+ *     responses:
+ *       200:
+ *         description: "Successful response"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Confirmation message"
+ *                   type: "string"
+ *             example:
+ *               message: "Category modified successfully"
+ *       404:
+ *         description: "Not found"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 message:
+ *                   title: "Error message"
+ *                   type: "string"
+ *             example:
+ *               message: "No matching category was found for your user."
+ */
 function registerUpdateCategory(app: Application) {
     app.put('/categories', authJwt.verifyToken, function (req, res, next) {
         const user_id = mongoose.Types.ObjectId.createFromHexString(req.body.token_user_id);
