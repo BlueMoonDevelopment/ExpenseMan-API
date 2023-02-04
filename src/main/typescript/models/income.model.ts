598bbd85acf6a36d3369f3a83d4c634b67fcb28b
TheJeterLP <joey.peter1998@gmail.com>
2023-02-04 19:38:26 +0100

    feat(income): add income and expense models

---
+++ src/main/typescript/models/income.model.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const IncomeSchema = new mongoose.Schema({
+    income_owner_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+    },
+    income_account_id: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+    },
+    income_name: {
+        type: String,
+        required: true,
+    },
+    income_desc: {
+        type: String,
+        default: '',
+    },
+    income_repeat_cycle: {
+        type: Number,
+        default: -1,
+    }
+});
+
+export const Income = mongoose.model('Income', IncomeSchema);
