99dc5c7427af0a48c301fd3e95cf4677fcae79b2
TheJeterLP <joey.peter1998@gmail.com>
2023-12-16 12:41:59 +0100

    remove not needed stuff

--- src/main/typescript/oauthhelper.ts
+++ src/main/typescript/oauthhelper.ts
@@ -1,51 +1,12 @@
-import { Auth } from 'googleapis';
-import { google_oauth_client_id, google_oauth_client_secret } from './config.json';
 import { Application } from 'express';
+import { jwtDecode } from 'jwt-decode';
 
-const oAuth2Client = new Auth.OAuth2Client({
-        clientId: google_oauth_client_id,
-        clientSecret: google_oauth_client_secret,
-        redirectUri: 'postmessage',
-    },
-);
 
 export function registerOAuthRoutes(app: Application) {
     app.post('/auth/google', (req, res) => {
-        // exchange code for tokens
-	console.log(req.headers);
-	console.log(req.body);
-	console.log('body.code: ' + req.body.code);
-	console.log('query.code: ' + req.query.code);
-	console.log('body.credential: ' + req.body.credential);
-        console.log('query.credential: ' + req.query.credential);
-        //const { tokens } = await oAuth2Client.getToken(req.body.code);
-        //console.log(tokens);
-        //res.json(tokens);
-	res.status(200).send();
+        const credential = req.body.credential;
+        const USER_CREDENTIAL = jwtDecode(credential);
+        res.status(200).json(USER_CREDENTIAL);
     });
 
-     app.get('/auth/google', async (req, res) => {
-        // exchange code for tokens
-	console.log("get");
-        console.log(req.query);
-        console.log('query.code: ' + req.query.code);
-        console.log('query.code: ' + req.query.code);
-        console.log('query.code: ' + req.query.code);
-        //const { tokens } = await oAuth2Client.getToken(req.body.code);
-        //console.log(tokens);
-        //res.json(tokens);
-        res.status(200).send();
-    });
-
-
-    app.post('/auth/google/refresh-token', async (req, res) => {
-        const user = new Auth.UserRefreshClient(
-            google_oauth_client_id,
-            google_oauth_client_secret,
-            req.body.refreshToken,
-        );
-        // optain new tokens
-        const { credentials } = await user.refreshAccessToken();
-        res.json(credentials);
-    });
 }
